4f4c7cadcf9cb2086d97b377c24ad7a940972391
Malshan Liyanage <pasindumalshanliyanage@gmail.com>
2025-07-23 22:51:37 +0530

    Update settings.py

--- nursery_backend/settings.py
+++ nursery_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nursery_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'accounts',
     'corsheaders',
-    'nursery_app',
+    
 ]
 
 MIDDLEWARE = [
