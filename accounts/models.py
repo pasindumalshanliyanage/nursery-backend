af489f11226f7b7c95cf372c8225f5a4ac20069b
Malshan Liyanage <pasindumalshanliyanage@gmail.com>
2025-07-24 10:38:21 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,8 @@ class UserProfile(models.Model):
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15)
-    ddress = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
     # Add any other fields you want, e.g.:
     # position = models.CharField(max_length=100)
     # date_joined = models.DateField(auto_now_add=True)
